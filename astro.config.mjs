f57a13e4fcb1c68e884aabfe71a0c74b368e8726
ultraub <r.barrett1501@gmail.com>
2022-08-27 20:32:09 +0000

    added bootstrap/created framework

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,29 @@
-import { defineConfig } from 'astro/config';
+import path from "path";
+
+import { fileURLToPath } from "url";
+
+import { defineConfig } from "astro/config";
+
+import { SITE } from "./src/config.mjs";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Astro uses this full URL to generate your sitemap and canonical URLs in your final build
+  site: SITE.domain,
+  base: "/",
+
+  output: "static",
+
+  integrations: [
+  ],
+
+  vite: {
+    resolve: {
+      alias: {
+        "~": path.resolve(__dirname, "./src"),
+      },
+    },
+  },
+});
---
+++ src/config.mjs
@@ -0,0 +1,7 @@
+export const SITE = {
+  org: "ASLU",
+  domain: "https://github.com/alexgoldsmith/something",
+  github: "https://github.com/alexgoldsmith/aslu",
+
+  postsPerPage: 6,
+};
