4b44ad8161ecf0cb3ec70948ca5456a3ca62bd3c
Alex Goldsmith <gold.alex.smith@gmail.com>
2022-08-28 17:48:24 +0000

    Functioning example with astro preview

--- src/config.mjs
+++ src/config.mjs
@@ -1,6 +1,6 @@
 export const SITE = {
   org: "ASLU",
-  domain: "https://github.com/alexgoldsmith/something",
+  domain: "https://github.com/",
   github: "https://github.com/alexgoldsmith/aslu",
 
   postsPerPage: 6,
---
+++ src/pages/lessons/lesson-2.md
@@ -0,0 +1,7 @@
+---
+layout: ~/layouts/LessonLayout.astro
+title: Lesson 2
+---
+
+# Lesson 2 
+This is a markdown page
