ef421d07dfe4bb62d253b9a5af81fae9bf50ac5d
Alex Goldsmith <46906584+alexgoldsmith@users.noreply.github.com>
2022-09-05 20:59:40 +0000

    checkpoint

--- scripts/process_dictionary.py
+++ scripts/process_dictionary.py
@@ -1,8 +1,6 @@
 import os
-import io
-import shutil
+import re
 from bs4 import BeautifulSoup as bs
-import unicodedata
 
 root_dir = os.getcwd()
 target_dir = f"{root_dir}/src/components/content/dictionary/"
@@ -11,8 +9,31 @@ for file in os.scandir(target_dir):
     with open(target_dir + file.name, 'r', encoding="utf-8") as f:
         contents = f.read()
         soup = bs(contents, 'html.parser')
-        for script in soup.find_all("script"):
+
+        for script in soup.find_all("script"):  # remove script tags
             script.decompose()
-        # clean_soup = unicodedata.normalize('NFKD', soup.text)
-    with open(target_dir + file.name, 'wb') as f:
-        f.write(soup.prettify("ascii"))
+
+        for obj in soup.find_all("object"):  # remove defunct flash objects
+            obj.decompose()
+
+        def findFoot(string):  # find defunct footer ptags
+            return re.compile("Want to help").search(string) or re.compile("Want even").search(string) or re.compile("Bandwidth").search(string) or re.compile("Dr. Bill's new").search(string) or re.compile("You can learn").search(string)
+
+        ptags = soup.find_all("p")
+        for ptag in ptags:
+            for font_tag in ptag.find_all(string=findFoot):
+                ptag.decompose()
+
+        for form in soup.find_all("form"):
+            form.decompose()  # remove old footer search bar
+
+        # TODO: could be more specific in the search criteria
+        def linkFilter(href):  # find links to .htm files
+            return href and re.compile(".htm").search(href)
+
+        for link in soup.find_all(href=linkFilter):  # replace references
+            newref = link["href"].rsplit("/", 1)[-1].replace(".htm", "")
+            link["href"] = "dictionary/" + newref
+
+    with open(target_dir + file.name, 'w', encoding="utf-8") as f:
+        f.write(soup.prettify(formatter=lambda s: s.replace('\xa0', ' ')))
